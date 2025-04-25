b61874c44a3db293885288c5173c60a5a951e8dd
adrdeatcu <150624496+adrdeatcu@users.noreply.github.com>
2025-04-25 13:05:44 +0300

    adding deleting feature + preparing the search filter option

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 let form = document.getElementById("addForm");
 let itemList = document.getElementById("unList");
 let textInput= document.getElementById("item-text");
+let filterInput= document.getElementById("filterInput")
 
 form.addEventListener('submit', addItem);
+itemList.addEventListener('click', deleteItem);
 
 function addItem(e)
 {
     e.preventDefault();
-    let newItem=textInput.value;
-    console.log(newItem);
+    let inputValue= textInput.value;
+    console.log(inputValue);
 
     let newLi=document.createElement("li");
-    newLi.textContent=newItem;
-
+    newLi.textContent=inputValue;
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+    deleteButton.classList.add("delete-button");
+    newLi.appendChild(deleteButton);
+    
     itemList.appendChild(newLi);
 
     textInput.value="";
-    
-    
-    
+}
+
+function deleteItem(e)
+{   
+    if(e.target.classList.contains("delete-button"))
+    {
+        let li= e.target.parentElement;
+        itemList.removeChild(li);
+    }
 }
